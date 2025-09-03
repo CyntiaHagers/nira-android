a2b804044fbf4030da46a23caf31980026216484
Israel <israelmfreitas12@gmail.com>
2025-09-03 14:13:31 -0300

    initial homescreen commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# nira-android
-Nira — aplicativo Android com foco social ESG que conecta mulheres em situação de violência a serviços de proteção, apoio psicológico e abrigos, promovendo acolhimento e autonomia.
--- app/src/main/java/br/com/fiap/nira/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/nira/ui/screens/HomeScreen.kt
@@ -1,9 +1,98 @@
 package br.com.fiap.nira.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.nira.ui.theme.NiraTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.nira.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
 
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF7939A4))
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 36.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_nira_app),
+                contentDescription = "image description",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .width(101.dp)
+                    .height(39.dp)
+            )
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Bem-vinda, Maria",
+                style = TextStyle(
+                    fontSize = 48.sp,
+                    lineHeight = 30.sp,
+                    fontFamily = FontFamily(Font(R.font.dongle_regular)),
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFFFFFFF),
+                )
+            )
+
+        }
+
+        Column (
+            modifier = Modifier.
+            align(Alignment.Center),
+        )
+        {
+            Button(
+                onClick = {}
+            ) { }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun HomeScreenPreview() {
+    NiraTheme {
+        HomeScreen()
+    }
 }
